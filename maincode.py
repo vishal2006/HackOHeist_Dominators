d77163de32ec304b680ec2c2f889159a6b7923ca
vishal2006 <vishalas323@gmail.com>
2021-10-16 22:43:47 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -5,12 +5,25 @@ import cv2
 import numpy as np
 import time
 from threading import Thread
+import importlib.util
 
 #----------------------------------------------------------------------------------------------------------------
 
-## Initialized variables
+### Initialized variables
 
 cap = cv2.VideoCapture(0)
+
+### Arguement parsing
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--modeldir',help='Enter the path of the DL model',required=True)
+parser.add_argument('--graph',help='Enter model name',default='detect.tflite')
+parser.add_argument('--labels',help='Enter the name of label',default='labelmap.txt')
+parser.add_argument('--threshold',help='Minimum threshold parameter',default=0.5)
+parser.add_argument('--resolution',help='Resolution of webcam',default='1280x720')
+parser.add_argument('--edgetpu',help='',action='store_true')
+
+
 
 #----------------------------------------------------------------------------------------------------------------
 # Functions
@@ -35,7 +48,6 @@ while(cap.isOpened()):
     break
 
 cap.release()
-
 cv2.destroyAllWindows()
 
 

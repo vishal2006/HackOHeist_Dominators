569150e2913601b80030e968319922c6c2e252c0
vishal2006 <vishalas323@gmail.com>
2021-10-16 17:56:57 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -3,4 +3,66 @@ import os
 import argparse
 import cv2
 import numpy as np
+import time
+from threading import Thread
 
+#----------------------------------------------------------------------------------------------------------------
+
+## Initialized variables
+
+
+#----------------------------------------------------------------------------------------------------------------
+# Functions
+
+# creating class for capturing images from video
+
+class VideoStream:
+    def __init__(self,resolution=(640,480),framerate=30):
+        self.stream = cv2.VideoCapture(0)
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(3,resolution[0])
+        ret = self.stream.set(4,resolution[1])
+            
+        (self.grabbed, self.frame) = self.stream.read()
+
+    # Variable to control when the camera is stopped
+        self.stopped = False
+
+    def start(self):
+    # Start the thread that reads frames from the video stream
+        Thread(target=self.update,args=()).start()
+        return self
+
+    def update(self):
+        # Keep looping indefinitely until the thread is stopped
+        while True:
+            # If the camera is stopped, stop the thread
+            if self.stopped:
+                # Close camera resources
+                self.stream.release()
+                return
+
+            # Otherwise, grab the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+    # Return the most recent frame
+        return self.frame
+
+    def stop(self):
+    # Indicate that the camera and thread should be stopped
+        self.stopped = True
+
+videoStream = VideoStream().start()
+time.sleep(0.5)
+
+while True:
+    frame = videoStream.read()
+    cv2.imshow('Surveillance System',frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+cv2.destroyAllWindows()
+videoStream.stop()

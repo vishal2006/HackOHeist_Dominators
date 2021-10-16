28995342bef57fa8bf9ebdcf87c197e0dda03df5
vishal2006 <vishalas323@gmail.com>
2021-10-16 17:57:06 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,14 @@
 import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    frame, ret = cap.read()
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+    cv2.imshow("image",frame)
+
+cv2.destroyAllWindows()
+cap.release()

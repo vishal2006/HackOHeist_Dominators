7864c917d3b54172d7152cce574e5d66407c7be4
vishal2006 <vishalas323@gmail.com>
2021-10-16 19:30:36 +0530

    video capture

--- maincode.py
+++ maincode.py
@@ -10,59 +10,33 @@ from threading import Thread
 
 ## Initialized variables
 
+cap = cv2.VideoCapture(0)
 
 #----------------------------------------------------------------------------------------------------------------
 # Functions
 
 # creating class for capturing images from video
 
-class VideoStream:
-    def __init__(self,resolution=(640,480),framerate=30):
-        self.stream = cv2.VideoCapture(0)
-        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-        ret = self.stream.set(3,resolution[0])
-        ret = self.stream.set(4,resolution[1])
-            
-        (self.grabbed, self.frame) = self.stream.read()
 
-    # Variable to control when the camera is stopped
-        self.stopped = False
+#------------------------------------------------------------------------------------------------------------------
 
-    def start(self):
-    # Start the thread that reads frames from the video stream
-        Thread(target=self.update,args=()).start()
-        return self
+while(cap.isOpened()):
 
-    def update(self):
-        # Keep looping indefinitely until the thread is stopped
-        while True:
-            # If the camera is stopped, stop the thread
-            if self.stopped:
-                # Close camera resources
-                self.stream.release()
-                return
+  ret, frame = cap.read()
+  if ret == True:
 
-            # Otherwise, grab the next frame from the stream
-            (self.grabbed, self.frame) = self.stream.read()
 
-    def read(self):
-    # Return the most recent frame
-        return self.frame
+    cv2.imshow('Frame',frame)
 
-    def stop(self):
-    # Indicate that the camera and thread should be stopped
-        self.stopped = True
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
 
-videoStream = VideoStream().start()
-time.sleep(0.5)
+  else: 
+    break
 
-while True:
-    frame = videoStream.read()
-    cv2.imshow('Surveillance System',frame)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-
+cap.release()
 
 cv2.destroyAllWindows()
-videoStream.stop()
+
+
+

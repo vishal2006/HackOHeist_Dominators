ad816d1f3c770da6265561a3ef57c53de9f989d1
vishal2006 <vishalas323@gmail.com>
2021-10-16 19:30:45 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,14 +1,25 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-while True:
-    frame, ret = cap.read()
+if (cap.isOpened()== False): 
+  print("Video capturing problem")
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+while(cap.isOpened()):
 
-    cv2.imshow("image",frame)
+  ret, frame = cap.read()
+  if ret == True:
+
+
+    cv2.imshow('Frame',frame)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+
+  else: 
+    break
+
+cap.release()
 
 cv2.destroyAllWindows()
-cap.release()

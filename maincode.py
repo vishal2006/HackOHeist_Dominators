7eb28a31f846cc3706202117575c37f781db4774
vishal2006 <vishalas323@gmail.com>
2021-10-17 10:38:33 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -77,6 +77,11 @@ width = input_details[0]['shape'][2]
 
 floating_model = (input_details[0]['dtype'] == np.float32)
 
+input_mean = 127.5
+input_std = 127.5
+
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
 
 
 #----------------------------------------------------------------------------------------------------------------
@@ -89,12 +94,42 @@ floating_model = (input_details[0]['dtype'] == np.float32)
 
 while(cap.isOpened()):
 
-  ret, frame = cap.read()
+  ret, frame1 = cap.read()
   if ret == True:
 
+    frame = frame1.copy()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height))
+    input_data = np.expand_dims(frame_resized, axis=0)
+    
+    if floating_model:
+            input_data = (np.float32(input_data) - input_mean) / input_std
 
-    cv2.imshow('Frame',frame)
+    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.invoke()
 
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0] 
+    classes = interpreter.get_tensor(output_details[1]['index'])[0] 
+    scores = interpreter.get_tensor(output_details[2]['index'])[0]
+
+    for i in range(len(scores)):
+        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+
+                ymin = int(max(1,(boxes[i][0] * imH)))
+                xmin = int(max(1,(boxes[i][1] * imW)))
+                ymax = int(min(imH,(boxes[i][2] * imH)))
+                xmax = int(min(imW,(boxes[i][3] * imW)))
+                
+                cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+                object_name = labels[int(classes[i])] 
+                label = '%s: %d%%' % (object_name, int(scores[i]*100)) 
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) 
+                label_ymin = max(ymin, labelSize[1] + 10) 
+                cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) 
+                cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) 
+                
+    cv2.imshow('Survelliance System', frame)
     if cv2.waitKey(25) & 0xFF == ord('q'):
       break
 

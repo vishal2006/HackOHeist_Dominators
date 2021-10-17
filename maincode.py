a94c3ed5297bd1f46fbb3fad932b62e0f9307602
vishal2006 <vishalas323@gmail.com>
2021-10-17 14:41:27 +0530

    gui added

--- maincode.py
+++ maincode.py
@@ -6,6 +6,13 @@ import numpy as np
 import time
 from threading import Thread
 import importlib.util
+import tkinter
+import glob
+from tkinter import *
+from PIL import ImageTk, Image
+import ctypes
+from tkinter import messagebox
+
 
 #----------------------------------------------------------------------------------------------------------------
 
@@ -87,54 +94,125 @@ freq = cv2.getTickFrequency()
 #----------------------------------------------------------------------------------------------------------------
 # Functions
 
-# creating class for capturing images from video
+def login(entry1,entry2,root):
+    if entry1.get()=="" or entry2.get()=="":
+        messagebox.showerror("Error","All fields are required",parent=root)
+    
+    elif entry1.get()!="Dominators" or entry2.get()!="123456":
+        messagebox.showerror("Error","Invalid Username/Password",parent=root)
+    
+    else:
+        root.destroy()
+        detect()
 
 
+def gui():
+
+    root = Tk()
+    root.title("Surveillance System")
+    root.geometry("1200x675")
+    root.minsize(1200,675)
+    root.maxsize(1200,675)
+
+    bg = ImageTk.PhotoImage(file="src\\bg.png")
+
+
+    my_canvas = Canvas(root, width=1200, height=675)
+    my_canvas.pack(fill="both",expand=True)
+
+    my_canvas.create_image(0,0, image=bg,anchor="nw")
+
+    text1 = my_canvas.create_text(700,100,fill="RoyalBlue1",font=("Segoe UI", 45, "bold"),text="Login Page")
+    text2 = my_canvas.create_text(650,280,fill="white",font=("Segoe UI", 15, "bold"),text="Username")
+    test3 = my_canvas.create_text(650,380,fill="white",font=("Segoe UI", 15, "bold"),text="Password")
+
+ 
+    entry1 = Entry(root,font=("Calibri 18 bold"),justify="center",bg="white",fg="black",borderwidth=2,width=25)
+    entry2 = Entry(root,font=("Calibri 18 bold"),justify="center",bg="white",fg="black",borderwidth=2,width=25,show="*")
+
+    my_button = Button(root,text="LOGIN",font=("Segoe UI", 18," bold"),command=login(entry1,entry2,root),width=10,fg="white",bg="RoyalBlue1",relief=GROOVE,activebackground="lightblue")
+
+    window1 = my_canvas.create_window(650,325,window=entry1)
+    window2 = my_canvas.create_window(650,400,window=entry2)
+
+    my_button.place(x=690,y=500)
+    entry1.place(x=600,y=300)
+    entry2.place(x=600,y=400)
+
+    root.mainloop()
+
+
+
+def detect():
+
+    while(cap.isOpened()):
+
+        ret, frame1 = cap.read()
+        if ret == True:
+
+            frame = frame1.copy()
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_resized = cv2.resize(frame_rgb, (width, height))
+            input_data = np.expand_dims(frame_resized, axis=0)
+            
+            if floating_model:
+                    input_data = (np.float32(input_data) - input_mean) / input_std
+
+            interpreter.set_tensor(input_details[0]['index'],input_data)
+            interpreter.invoke()
+
+            boxes = interpreter.get_tensor(output_details[0]['index'])[0] 
+            classes = interpreter.get_tensor(output_details[1]['index'])[0] 
+            scores = interpreter.get_tensor(output_details[2]['index'])[0]
+
+            for i in range(len(scores)):
+                if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+
+                        ymin = int(max(1,(boxes[i][0] * imH)))
+                        xmin = int(max(1,(boxes[i][1] * imW)))
+                        ymax = int(min(imH,(boxes[i][2] * imH)))
+                        xmax = int(min(imW,(boxes[i][3] * imW)))
+                        
+                        cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+                        object_name = labels[int(classes[i])] 
+                        label = '%s: %d%%' % (object_name, int(scores[i]*100)) 
+                        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) 
+                        label_ymin = max(ymin, labelSize[1] + 10) 
+                        cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) 
+                        cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) 
+
+            
+            frame2 = frame.copy()
+            frame_rgb = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
+            frame_resized = cv2.resize(frame_rgb, (1280,720))
+            input_data = np.expand_dims(frame_resized, axis=0)
+            bgui = cv2.imread("src\\bgui.jpg")
+            image1 = cv2.resize(bgui, (1600, 900))
+            framecamera = cv2.resize(frame2, (878, 456))
+            
+            image1[130:130+framecamera.shape[0],120:120+framecamera.shape[1]] = framecamera
+            
+            cv2.putText(image1,"State:",(780,760),cv2.FONT_HERSHEY_TRIPLEX,1,(255,255,255),2,cv2.LINE_AA)
+            cv2.putText(image1,"Normal",(780,820),cv2.FONT_HERSHEY_TRIPLEX,1,(255,255,255),2,cv2.LINE_AA)
+            cv2.putText(image1,"Soldier:",(200,780),cv2.FONT_HERSHEY_TRIPLEX,1,(255,255,255),2,cv2.LINE_AA)
+            cv2.putText(image1,"False",(200,820),cv2.FONT_HERSHEY_TRIPLEX,1,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(image1,"Objects Detected:",(1200,130),cv2.FONT_HERSHEY_TRIPLEX,1,(255,255,255),2,cv2.LINE_AA)
+            cv2.putText(image1,"GPS Coordinates:",(1160,770),cv2.FONT_HERSHEY_TRIPLEX,0.8,(255,255,255),2,cv2.LINE_AA)
+            cv2.putText(image1,"21° 2' 27.528'' N 75° 3' 26.748'' E",(1160,800),cv2.FONT_HERSHEY_TRIPLEX,0.5,(255,255,255),1,cv2.LINE_AA)
+            image1 = cv2.resize(image1, (1200, 700))
+            cv2.imshow('Survelliance System', image1)
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        else: 
+            break
 #------------------------------------------------------------------------------------------------------------------
 
-while(cap.isOpened()):
+### Main
 
-  ret, frame1 = cap.read()
-  if ret == True:
-
-    frame = frame1.copy()
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = cv2.resize(frame_rgb, (width, height))
-    input_data = np.expand_dims(frame_resized, axis=0)
-    
-    if floating_model:
-            input_data = (np.float32(input_data) - input_mean) / input_std
-
-    interpreter.set_tensor(input_details[0]['index'],input_data)
-    interpreter.invoke()
-
-    boxes = interpreter.get_tensor(output_details[0]['index'])[0] 
-    classes = interpreter.get_tensor(output_details[1]['index'])[0] 
-    scores = interpreter.get_tensor(output_details[2]['index'])[0]
-
-    for i in range(len(scores)):
-        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
-
-                ymin = int(max(1,(boxes[i][0] * imH)))
-                xmin = int(max(1,(boxes[i][1] * imW)))
-                ymax = int(min(imH,(boxes[i][2] * imH)))
-                xmax = int(min(imW,(boxes[i][3] * imW)))
-                
-                cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
-
-                object_name = labels[int(classes[i])] 
-                label = '%s: %d%%' % (object_name, int(scores[i]*100)) 
-                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) 
-                label_ymin = max(ymin, labelSize[1] + 10) 
-                cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) 
-                cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) 
-                
-    cv2.imshow('Survelliance System', frame)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-      break
-
-  else: 
-    break
+gui()
 
 cap.release()
 cv2.destroyAllWindows()
